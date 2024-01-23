3fb43aedad8907402a18eeb1a7ed300af7254fd9
skyteme <145546791+skyteme@users.noreply.github.com>
2024-01-23 11:06:36 +0900

    회원가입 추가
    이메일 인증
    주민등록번호 인증
    패스워드 확인
    및 디자인 변경

--- uswine/src/main/uswinefront/src/pages/JoinPage.jsx
+++ uswine/src/main/uswinefront/src/pages/JoinPage.jsx
@@ -6,97 +6,228 @@ import PopupPostCode from "./Popup"
 
 export default function JoinPage(){
     const [email,setEmail] = useState('')
-    const [password,setPassword] = useState('')
-    const [nickname,setNickName] = useState('')
-    const [address,setAddress] = useState('')
-    const [phone,setPhone] = useState('')
-    const [isPopupOpen, setIsPopupOpen] = useState(false)
 
-    function openPostCode(){
-        setIsPopupOpen(true)
+    const [openEmailCheck,setOpenEmailCheck] = useState(false)
+    const [emailCheck,setEmailCheck] = useState('')
+
+    const [inputNum,setInputNum] = useState('')
+    const [isEmailCheck,setIsEmailCheck] = useState('false')
+
+    const [password,setPassword] = useState('')
+    const [passwordCheck,setPasswordCheck] = useState('')
+    const [nickname,setNickName] = useState('')
+    const [postcode,setPostCode] = useState('')
+    const [address,setAddress] = useState('')
+    const [detailAddress,setDetailAddress] = useState('')
+
+    const [frontResident,setFrontResident] = useState('')
+    const [backResident,setBackResident] = useState('')
+
+    const [residentCheck,setResidentCheck] = useState('false')
+    const [phone,setPhone] = useState('')
+    const year = new Date().getFullYear()
+
+    function checkResident(){
+        let age = parseInt(frontResident.slice(0,2), 10)
+        if(age>=24){
+            age+=1900
+        }else{
+            age+=2000
+        }
+        console.log(year-age)
+        if(year-age>=18){
+            alert("성인입니다")
+            setResidentCheck('true')
+        }else{
+            alert("성인이 아닙니다")
+            setResidentCheck('false')
+        }
     }
 
-    function closePostCode(){
-        setIsPopupOpen(false)
+
+    async function sendEmail(){
+        try{
+            const response = await axios.post('http://localhost:8080/api/user/sendEmail',{
+                address:email
+            })
+            setOpenEmailCheck('true')
+            setEmailCheck(response.data)
+        }catch (error) {
+            console.error('Error:', error);
+        }
+    }
+    function checkNumber(num){
+        if(num === emailCheck){
+            setOpenEmailCheck('false')
+            setIsEmailCheck('true')
+        }else{
+            alert('인증번호가 틀렸습니다 다시입력해주세요')
+            sendEmail()
+        }
     }
 
     async function handleSubmit() {
-        setPhone(phone.replace(/\D/g, ''))
-        if(phone === 10 || phone ===11){
-            setPhone(phone.replace(/(\d{3})(\d{3,4})(\d{3,4})/, '$1-$2-$3'))
-        }
-    
-        try {
-            const response = await axios.post('http://localhost:8080/api/user/join', {
-                email: email,
-                password: password,
-                nickname: nickname,
-                address: address,
-                phone: phone
-            });
-    
-            console.log('Server response:', response.data);
-        } catch (error) {
-            console.error('Error:', error);
+        
+        
+        const textAddress = postcode+" "+address+" "+detailAddress
+        if(isEmailCheck === 'true'){
+            if(residentCheck ==='true'){
+                if(password === passwordCheck){
+                    try {
+                        const response = await axios.post('http://localhost:8080/api/user/join', {
+                            email: email,
+                            password: password,
+                            nickname: nickname,
+                            address: textAddress,
+                            phone: phone
+                        });
+                        
+                        console.log('Server response:', response.data);
+                        if(response.data === "join successful"){
+                            window.location.href='/LoginPage';
+                        }else{
+                            window.location.href='/JoinPage';
+                        }
+                    } catch (error) {
+                        console.error('Error:', error);
+                    }
+                }else{
+                    alert("비밀번호가 일치하지 않습니다.")
+                    document.getElementById('passwordCheck').focus();
+                }
+            }else{
+                alert('주민등록번호 인증을 해주세요')
+                document.getElementById('frontResident').focus();
+            }
+        }else{
+            alert('이메일 인증을 해주세요.')
+            document.getElementById('email').focus();
         }
     }
     return(
         <div id="joinBlock">
-            <h1>회원가입</h1>
             <div>
                 <div id="inputIdBlock">
-                    <Form.Label htmlFor="inputJoinId">이메일</Form.Label>
                     <Form.Control
                         type="email"
                         id="inputJoinId"
                         name="email"
+                        placeholder="이메일"
                         onChange={(e)=>setEmail(e.target.value)}
                     />
+                        <div id="numCheckBlock">
+                        {openEmailCheck && (
+                            <>
+                                <Form.Control
+                                    type="text"
+                                    id="numCheck"
+                                    name="numCheck"
+                                    placeholder="인증번호 입력"
+                                    onChange={(e)=>setInputNum(e.target.value)}
+                                />
+                                <Button id="numCheckButton" variant="light" onClick={()=>checkNumber(inputNum)}>인증</Button>
+                            </>
+                         )} 
+                            <Button id="checkEmailButton" variant="light" onClick={sendEmail} >
+                                인증번호 발송
+                            </Button>
+                        </div>
+                    
+                </div>
+                <div id="inputResidentBlock">
+                    <div id="inputResident">
+                        <Form.Control
+                            type="text"
+                            id="frontResident"
+                            name="frontresident"
+                            placeholder="주민등록번호 앞 6자리"
+                            maxLength={6}
+                            onChange={(e)=>{
+                                const inputValue = e.target.value.slice(0,6)
+                                setFrontResident(inputValue)
+                            }}
+                        />
+                        -
+                        <Form.Control
+                            type="password"
+                            id="backResident"
+                            name="backresident"
+                            maxLength={1}
+                            onChange={(e)=>{
+                                const inputValue = e.target.value.slice(0,1)
+                                setBackResident(inputValue)
+                            }}
+                        /><h6>● ● ● ● ● ●</h6>
+                    </div>
+                    <Button id="checkResidentButton" variant="light" onClick={checkResident} >
+                        인증
+                    </Button>
                 </div>
                 <div id="inputPasswordBlock">
-                    <Form.Label htmlFor="inputJoinPassword">비밀번호</Form.Label>
                     <Form.Control
                         type="password"
                         id="inputJoinPassword"
                         name="password"
+                        placeholder="패스워드"
                         onChange={(e)=>setPassword(e.target.value)}
                     />
                 </div>
+                <div id="inputPasswordCheckBlock">
+                    <Form.Control
+                        type="password"
+                        id="inputJoinPasswordCheck"
+                        name="password"
+                        placeholder="패스워드확인"
+                        onChange={(e)=>setPasswordCheck(e.target.value)}
+                    />
+                </div>
+                <div id="checkMessage">
+                    {(password !== '' && passwordCheck !== '') ? (  // password와 passwordCheck가 빈 문자열이 아닌 경우에만 렌더링
+                        password !== passwordCheck ? (
+                        <h6 style={{ color: 'red' }}>비밀번호가 일치하지 않습니다</h6>
+                        ) : (
+                        <h6 style={{ color: 'green' }}>비밀번호가 일치합니다</h6>
+                        )
+                    ) : null}
+                </div>
+
                 <div id="inputNickNameBlock">
-                    <Form.Label htmlFor="inputJoinNickName">닉네임</Form.Label>
                     <Form.Control
                         type="text"
                         id="inputJoinNickName"
                         name="NickName"
+                        placeholder="닉네임"
                         onChange={(e)=>setNickName(e.target.value)}
                     />
                 </div>
                 <div id="inputAddressBlock">
-                    <Form.Label htmlFor="inputJoinAdress">주소</Form.Label>
+                    <div id ='postCodeBlock'>
+                        <Form.Control
+                            type="text"
+                            id="postcode"
+                            placeholder="우편번호"
+                        />
+                        <PopupPostCode setPostCode={setPostCode} setAddress={setAddress} />    
+                    </div>
                     <Form.Control
                         type="text"
-                        id="inputJoinAddress"
-                        name="Address"
-                        value={address}
-                        onChange={(e)=>setAddress(e.target.value)}
+                        id="address"
+                        placeholder="주소"
                     />
-                    <div id="FindAddressBtnBlock">
-                        <Button variant="light" onClick={openPostCode} >
-                            우편번호 검색
-                        </Button>
-                        <div id='popupDom'>
-                            {isPopupOpen && (
-                                <PopupPostCode setAddress={setAddress} onClose={closePostCode} />
-                            )}
-                        </div>
-                    </div>
+                    <Form.Control
+                        type="text"
+                        id="detailAddress"
+                        placeholder="상세주소"
+                        onChange={(e)=>setDetailAddress(e.target.value)}
+                    />
                 </div>
                 <div id="inputPhoneBlock">
-                    <Form.Label htmlFor="inputJoinPhone">휴대전화</Form.Label>
+                    
                     <Form.Control
                         type="text"
                         id="inputJoinPhone"
                         name="Phone"
+                        placeholder="휴대전화번호 - 빼고 넣어주세요"
                         onChange={(e)=>setPhone(e.target.value)}
                     />
                 </div>
--- uswine/src/main/uswinefront/src/pages/Popup.jsx
+++ uswine/src/main/uswinefront/src/pages/Popup.jsx
@@ -1,45 +1,50 @@
-import React from 'react';
-import DaumPostcode from "react-daum-postcode";
- 
-export default function PopupPostCode(props) {
-	// 우편번호 검색 후 주소 클릭 시 실행될 함수, data callback 용
-    const handlePostCode = (data) => {
-        let fullAddress = data.address;
-        let extraAddress = ''; 
-        
-        if (data.addressType === 'R') {
-          if (data.bname !== '') {
-            extraAddress += data.bname;
-          }
-          if (data.buildingName !== '') {
-            extraAddress += (extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName);
-          }
-          fullAddress += (extraAddress !== '' ? ` (${extraAddress})` : '');
-        }
-        console.log(data)
-        console.log(fullAddress)
-        console.log(data.zonecode)
-        props.setAddress(fullAddress);
-        
-        props.onClose()
-    }
+ import { useEffect } from "react";
+ import { Button } from "react-bootstrap";
+export default function PopupPostCode({setPostCode,setAddress}) {
+  useEffect(() => {
+    // 다음 우편번호 서비스 라이브러리 로드
+    const script = document.createElement('script');
+    script.src = 'https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js';
+    script.async = true;
+    document.head.appendChild(script);
 
-    const postCodeStyle = {
-        display: "block",
-        position: "absolute",
-        top: '50%',
-        left: '50%',
-        transform:'translate(-50%,-50%)',
-        width: "600px",
-        height: "600px",
-        padding: "7px",
-        border: "2px solid #666"
-      };
- 
-    return(
-        <div>
-            <DaumPostcode style={postCodeStyle} onComplete={handlePostCode} /> 
-        </div>
-    )
+    script.onload = () => {
+      // 다음 우편번호 서비스 라이브러리 로드 후 실행되어야 할 코드
+      if (window.daum && window.daum.Postcode) {
+        console.log('다음 우편번호 서비스 라이브러리가 로드되었습니다.');
+      } else {
+        console.error('다음 우편번호 서비스 라이브러리가 로드되지 않았습니다.');
+      }
+    };
+
+    return () => {
+      // 컴포넌트 언마운트 시에 스크립트 제거
+      document.head.removeChild(script);
+    };
+  }, []);
+
+    const execDaumPostcode = () => {
+      new window.daum.Postcode({
+        oncomplete: function (data) {
+          var addr = ''; // 주소 변수
+          if (data.userSelectedType === 'R') {
+            addr = data.roadAddress;
+          } else {
+            addr = data.jibunAddress;
+          }
+          setPostCode(data.zonecode);
+          setAddress(addr);
+          document.getElementById('postcode').value=data.zonecode
+          document.getElementById('address').value=addr
+        },
+      }).open();
+      
+    };
+  
+    return (
+      <>
+        <Button id="openPopup" type="button" onClick={execDaumPostcode}
+        variant="light">우편번호 찾기</Button><br />
+      </>
+    );
 }
-
