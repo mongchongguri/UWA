21a7058cdf493f33ad6b5157378c65e954def3a5
mongchongguri <125566993+mongchongguri@users.noreply.github.com>
2024-01-26 17:34:20 +0900

    올삭제

--- uswine/src/main/java/com/uwa/uswine/main/board/BoardRepository.java
+++
@@ -1,6 +0,0 @@
-package com.uwa.uswine.main.board;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<BoardEntity,Object> {
-}
--- uswine/src/main/java/com/uwa/uswine/mypage/sellerRigist/SellerRegistRepository.java
+++
@@ -1,8 +0,0 @@
-package com.uwa.uswine.mypage.sellerRigist;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SellerRegistRepository extends JpaRepository<SellerRegistEntity, Object> {
-}
