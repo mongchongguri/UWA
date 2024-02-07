755b8369990c25933bbf4c66e9002740e15c2673
skyteme <145546791+skyteme@users.noreply.github.com>
2024-02-07 10:49:43 +0900

    zzz

--- uswine/src/main/uswinefront/src/function/MarkUp.jsx
+++
@@ -1,5 +0,0 @@
-function MarkUp({ MakrDownData }) {
-  return <div dangerouslySetInnerHTML={{ __html: `${MakrDownData}` }} />;
-}
-
-export default MarkUp;
--- uswine/src/main/uswinefront/src/pages/admin/UserListBody.jsx
+++
@@ -1,5 +0,0 @@
-export default function UserListBody(userList){
-    return(
-        ''
-    )
-}
