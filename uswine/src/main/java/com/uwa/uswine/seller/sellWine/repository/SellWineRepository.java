3f7c32a5232de270dab1c022e34191a08e499fba
skyteme <145546791+skyteme@users.noreply.github.com>
2024-02-16 16:40:59 +0900

    추합 추가

--- uswine/src/main/java/com/uwa/uswine/seller/sellWine/repository/SellWineRepository.java
+++ uswine/src/main/java/com/uwa/uswine/seller/sellWine/repository/SellWineRepository.java
@@ -1,11 +1,17 @@
 package com.uwa.uswine.seller.sellWine.repository;
 
 import com.uwa.uswine.seller.sellWine.entity.SellWineEntity;
+
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
 public interface SellWineRepository extends ReactiveMongoRepository<SellWineEntity,Object> {
     Mono<SellWineEntity> findById(String id);
+    Flux<SellWineEntity> findAllById(List<String> mongoId);
 }
